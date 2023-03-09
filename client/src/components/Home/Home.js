8df1d540f13b390e4c71f763437883391d9a4dfb
Rosen Spiridonov <rspiridonov3@gmail.com>
2023-03-09 16:19:46 +0200

    Initial commit

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+function Home() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:5500/home/test')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <>
+            <h1>This is my home page brother</h1>
+            <h2>What should I do next</h2>
+            <div>
+                {data ? (
+                    data.map(item => (
+                        <div key={item.id}>
+                            <h3>{item.name}</h3>
+                            <p>{item.description}</p>
+                        </div>
+                    ))
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
+        </>
+    );
+}
+
+export default Home;
